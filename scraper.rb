87c452f98651fd8645997b759768f674de0ef426
kelpenhagen <kelpenhagen@gmail.com>
2015-09-04 13:42:13 +1000

    Print one record of table
    Our FIRST record - high five!!

--- scraper.rb
+++ scraper.rb
@@ -1,16 +1,28 @@
+# make sure you run "bundle exec irb" not just irb
+
+
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
+require 'scraperwiki'
+require 'mechanize'
+
+agent = Mechanize.new
 #
 # # Read in a page
-# page = agent.get("http://foo.com")
-#
+page = agent.get("http://www.rfs.nsw.gov.au/fire-information/fdr-and-tobans")
+
+fire_area = {
+	name: page.at(".danger-ratings-table").at(:tbody).search(:tr).first.search(:td).first.text,
+	today_fire_d: page.at(".danger-ratings-table").at(:tbody).search(:tr).first.search(:td)[1].text,
+	today_toban: page.at(".danger-ratings-table").at(:tbody).search(:tr).first.search(:td)[2].text,
+	tomorrow_fire_d: page.at(".danger-ratings-table").at(:tbody).search(:tr).first.search(:td)[3].text,
+	tomorrow_toban: page.at(".danger-ratings-table").at(:tbody).search(:tr).first.search(:td)[4].text,
+	council: page.at(".danger-ratings-table").at(:tbody).search(:tr).first.search(:td)[5].text
+}
+
 # # Find somehing on the page using css selectors
-# p page.at('div.content')
+p fire_area
 #
 # # Write out to the sqlite database using scraperwiki library
 # ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
